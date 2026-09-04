e68357eeb580bf493f2094533c40b4ae0170184c
Matt <Matt@JESSE-DELL>
2026-09-05 00:29:00 +0200

    Add project files.

---
+++ FuelLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fuel_Loger
+{
+    internal class FuelLog
+    {
+        public DateTime LogDate { get; set; }
+        public float Litres {  get; set; }
+        public float Cost { get; set; }
+
+        public FuelLog(DateTime date,float litres, float cost)
+        {
+            LogDate = date;
+            Litres = litres;
+            Cost = cost;
+        }
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Fuel_Loger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string FILE_PATH = "C:\\Users\\Matt\\Desktop\\FuelLoger\\Fuel_Loger\\Fuel_Logs.txt";
+
+        public MainWindow()
+        {
+            if (File.Exists(FILE_PATH) != true)
+            {
+                MessageBox.Show("File not found","Missing File",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            using (StreamReader reader = new StreamReader(FILE_PATH))
+            {
+                string sLine;
+
+                while ((sLine = reader.ReadLine()) != null)
+                {
+                    int ipos = sLine.IndexOf("#");
+                    string strDate = sLine.Substring(0, ipos);
+                    DateTime date = DateTime.ParseExact(strDate, "yyyyMMdd", CultureInfo.InvariantCulture);
+                    sLine = sLine.Remove(0, ipos + 1);
+                    ipos = sLine.IndexOf("#");
+                    string sAmount = sLine.Substring(0, ipos);
+                    sLine = sLine.Remove(0, ipos + 1);
+                    string sCost = sLine;
+                    FuelLog log = new FuelLog(date,float.Parse(sAmount),float.Parse(sCost));
+                }
+            }
+
+            InitializeComponent();
+        }
+    }
+}
